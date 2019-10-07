8e7a9e583bda1d824e5a8237a40c916de8fe5090
Parisa Hemmati <Phemmati@usfca.edu>
2019-10-07 10:38:58 -0700

    first commit'

---
+++ src/BankAccount3.java
@@ -0,0 +1,42 @@
+public class BankAccount3 implements Comparable {
+    public float balance;
+    public float rate;
+
+
+    public void processYear(){
+
+        float interest = balance * rate;
+        balance = balance + interest;
+
+    }
+
+    public void processNYears(int n){
+        int i = 0;
+        while (i < n) {
+            processYear();
+            i = i + 1;
+        }
+    }
+
+    public static void main(String [] args) {
+
+        BankAccount3 account = new BankAccount3(1000,.1f);
+        BankAccount3 account2 = new BankAccount3(1000,.1f);
+        account.processNYears(10);
+        account.compareTo(account2);
+        System.out.println("balance after years is: " + account.balance);
+
+    }
+    public BankAccount3(float balance,float rate){
+        this.balance = balance;
+        this.rate = rate;
+    }
+
+    public int compareTo(Object o){
+        if this.balance ==
+    }
+
+
+
+}
+
